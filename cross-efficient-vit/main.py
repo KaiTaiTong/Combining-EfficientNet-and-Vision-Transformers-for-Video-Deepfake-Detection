6720f6e8844daf09958daefe47c63612fcbb4237
Kaitai Tong <kaitai@alumni.ubc.ca>
2022-12-11 22:04:47 -0800

    Add for benchmark test (cross-eff-vit)

---
+++ cross-efficient-vit/main.py
@@ -0,0 +1,87 @@
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.metrics import auc
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+
+import os
+import cv2
+import numpy as np
+import torch
+from torch import nn, einsum
+from sklearn.metrics import plot_confusion_matrix
+
+# from utils import get_method, check_correct, resize, shuffle_dataset, get_n_params
+import torch.nn as nn
+import torch.nn.functional as F
+from functools import partial
+from cross_efficient_vit import CrossEfficientViT
+# from utils import transform_frame
+import glob
+from os import cpu_count
+import json
+from multiprocessing.pool import Pool
+import pandas as pd
+from tqdm import tqdm
+from multiprocessing import Manager
+# from utils import custom_round, custom_video_round
+from albumentations import Resize, Compose, RandomBrightnessContrast, \
+    HorizontalFlip, FancyPCA, HueSaturationValue, OneOf, ToGray, \
+    ShiftScaleRotate, ImageCompression, PadIfNeeded, GaussNoise, GaussianBlur, Rotate
+# from .transforms.albu import IsotropicResize
+import yaml
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--filter_type', type=str)
+parser.add_argument('--mixing_type', type=str)
+args = parser.parse_args()
+
+filter_type = args.filter_type
+mixing_type = args.mixing_type
+
+config = './configs/architecture.yaml'
+model_path = './cross_efficient_vit.pth'
+
+device = "cuda"
+
+with open(config, 'r') as ymlfile:
+    config = yaml.safe_load(ymlfile)
+model = CrossEfficientViT(config=config)
+model.load_state_dict(torch.load(model_path))
+model.eval()
+model = model.cuda()
+
+# filter_type = 'filtered'
+# mixing_type = 'lip'
+method = 'cross-efficient-vit'
+
+parent_dir = '../../local-editing-dataset/experiment/test/{}/{}'.format(
+    filter_type, mixing_type)
+
+all_imgs_dir = [
+    os.path.join(parent_dir, i) for i in os.listdir(parent_dir)
+    if (i.endswith('.png') or i.endswith('.jpg'))
+]
+
+img_size = config['model']['image-size']
+
+preds_list = []
+imgs_path_list = []
+for img_dir in tqdm(all_imgs_dir):
+    frame = cv2.cvtColor(cv2.imread(img_dir), cv2.COLOR_BGR2RGB)
+    resize = Resize(width=img_size, height=img_size)
+    frame = resize(image=frame)['image']
+    frame = torch.tensor(frame).to(device)
+    with torch.no_grad():
+        # predict for frame
+        # channels first
+        frame = frame.permute(2, 0, 1)
+        frame = frame.float()
+        prediction = model(frame.unsqueeze(0))
+        preds = torch.sigmoid(prediction)
+        preds_list.append(preds.cpu().numpy()[0][0])
+        imgs_path_list.append(img_dir)
+
+df = pd.DataFrame({'img_dir': imgs_path_list, 'pred': preds_list})
+df.to_csv('{}_{}_{}.csv'.format(method, filter_type, mixing_type))
---
+++ cross-efficient-vit/sbatch/sbatch_unfiltered_eye.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=eval
+#SBATCH --account=def-panos
+#SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --mem=32G
+#SBATCH --time=1:00:00
+
+module load StdEnv/2020
+module load gentoo/2020
+module load python/3.7.7
+module load intel/2020.1.217  cuda/11.4
+
+source ~/projects/def-panos/alantkt/envs/local-editing-dataset/bin/activate
+cd ~/projects/def-panos/alantkt/codes/Combining-EfficientNet-and-Vision-Transformers-for-Video-Deepfake-Detection/cross-efficient-vit
+
+python main.py --filter_type "unfiltered" --mixing_type "eye"
+echo "End"
